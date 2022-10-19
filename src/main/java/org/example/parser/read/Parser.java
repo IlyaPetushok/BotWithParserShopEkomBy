53ce72442cc02c8f05945939a728bd09465778c6
Petushok Ilya <a3310010752@mail.ru>
2022-10-19 23:24:16 +0300

    clear Code

--- src/main/java/org/example/parser/read/Parser.java
+++ src/main/java/org/example/parser/read/Parser.java
@@ -1,41 +1,37 @@
 package org.example.parser.read;
 
+import org.example.parser.entity.Item;
 import org.example.parser.entity.ServerAttribute;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+
 
 public class Parser {
+    private static final String HOME_PAGE = "https://ekom.uz";
 
     public Document getDocument(String url) throws IOException {
-//        String url = "https://ekom.uz";Главная страница
-//        String url = "https://ekom.uz/setevoe-oborudovanie/aktivnoe-oborudovanie/servera/";
-//                "https://ekom.uz/setevoe-oborudovanie/aktivnoe-oborudovanie/servera/";
-        //"https://ekom.uz/setevoe-oborudovanie";
-        Document document = Jsoup.parse(new URL(url), 4000);
-//        System.out.println(document);
-        return document;
+        return Jsoup.parse(new URL(url), 4000);
     }
 
-
-    public void itemsShow(String url) throws IOException {
+    public List<Item> itemsShow(String url, String numberPage) throws IOException {
+        if (Integer.parseInt(numberPage) > 1) {
+            url += "/page-" + numberPage + "/";
+        }
         Document document = getDocument(url);
         Element body = document.body();
         Element quantityPage;
+        List<Item> itemsList = new ArrayList<>();
         String strQuantityPage;
         if (body.select("div.ty-pagination__bottom").select("a.ty-pagination__range").size() > 0) {
             quantityPage = body.select("div.ty-pagination__bottom").select("a.ty-pagination__range").first();
             String[] arr = quantityPage.text().split("- ");
             strQuantityPage = arr[arr.length - 1];
         } else if (body.select("div.ty-pagination__bottom").select("a").size() > 0) {
-//            quantityPage=body.select("div.ty-pagination__bottom").select("a");
             Elements page = body.select("div.ty-pagination__bottom").select("a");
             quantityPage = page
                     .stream()
@@ -48,85 +44,82 @@ public class Parser {
         }
         System.out.println("quantity page:" + strQuantityPage);
         Element itemsBlock = body.select("div.grid-list").first();
-        Element items = itemsBlock.getElementById("categories_view_pagination_contents");
-        for (Element item : items.select("div.ty-column3").select("div.ut2-gl__item ")) {
-            //photo
-            //там есть две фотки разных размеров
-            System.out.println("photo: " + item.select("div.ut2-gl__image").select("img.ty-pict").attr("data-src"));
-            //name
-            System.out.println("name: " + item.select("div.ut2-gl__name").text());
-            //status(в наличии,нет в наличии ,предзаказ)
-            System.out.println("status: " + item.select("div.ut2-gl__amount").text());
-            //цена (цифры ,свяжитесь с нами для уточнения)
-            System.out.println("price: " + item.select("div.ut2-gl__price").text());
+        if (itemsBlock != null) {
+            Element items = itemsBlock.getElementById("categories_view_pagination_contents");
+            for (Element item : items.select("div.ty-column3").select("div.ut2-gl__item ")) {
+                String photo = item.select("div.ut2-gl__image").select("img.ty-pict").attr("data-src");
+                String name = item.select("div.ut2-gl__name").text();
+                String status = item.select("div.ut2-gl__amount").text();
+                String price = item.select("div.ut2-gl__price").text();
+                itemsList.add(new Item(photo, name, status, price));
+            }
+            return itemsList;
         }
-
+        return null;
     }
 
     public List<ServerAttribute> showCategory() throws IOException {
-        int i = 1;
         List<ServerAttribute> serverAttributes = new ArrayList<>();
-        List<String> nameDownCatalogList = null;
-        String newUrl;
-        Scanner in = new Scanner(System.in);
-        Document document = getDocument("https://ekom.uz");
+        List<String> nameDownCatalogList;
+        Document document = getDocument(HOME_PAGE);
         Element body = document.body();
-        Elements catalogs = body.select("ul[class=ty-menu__items cm-responsive-menu]");//сами ссылки на котологи
-//        System.out.println("Выберите каталог");
-        for (Element catalog : catalogs.select("li[class=ty-menu__item cm-menu-item-responsive first-lvl]")) {
+        for (Element catalog : getCatalog(body)) {
             String nameCatalog = catalog.select("span[class=menu-lvl-ctn exp-wrap]").select("bdi").text();
-            System.out.println(i++ + ":" + nameCatalog);//название католога
             nameDownCatalogList = new ArrayList<>();
             for (Element downCatalog : catalog.select("div.second-lvl")) {
                 nameDownCatalogList.add(downCatalog.select("div.ty-menu__submenu-item-header").select("bdi").text());
-//                System.out.println("\t" + downCatalog.select("div.ty-menu__submenu-item-header").select("bdi").text());
             }
             serverAttributes.add(new ServerAttribute(nameCatalog, nameDownCatalogList));
         }
-//        i = 1;
-//        int indexCatalog = in.nextInt();
-//        Elements inputCatalog = catalogs
-//                .select("li[class=ty-menu__item cm-menu-item-responsive first-lvl]")
-//                .get(indexCatalog - 1)
-//                .select("div[class=ty-menu__submenu-col]");//название блока
-//        for (Element blockInputCatalog : inputCatalog.select("div[class=second-lvl]")) {
-//
-//            System.out.print("\t\t");
-//            System.out.println(i++ + ":" + blockInputCatalog.select("div.ty-menu__submenu-item-header").select("bdi").text());
-//        }
-//        i = 1;
-//        int indexDownCatalog = in.nextInt();
-//        Elements hrefItems = inputCatalog.select("div[class=second-lvl]").get(indexDownCatalog - 1).select("div.ty-menu__submenu-item");
-//        for (Element hrefItem : hrefItems) {
-//            System.out.println("\t\t\t\t" + i++ + " " + hrefItem.select("a").select("span").text());
-//            System.out.println("\t\t\t\t" + hrefItem.select("a").attr("href"));
-//        }
-//        int indexHref = in.nextInt();
-//        return hrefItems
-//                .get(indexHref - 1)
-//                .select("a")
-//                .attr("href");
         return serverAttributes;
     }
 
     public List<ServerAttribute> getInputDownCatalog(String nameCatalog, String id) throws IOException {
-        Document document = getDocument("https://ekom.uz");
+        Document document = getDocument(HOME_PAGE);
         Element body = document.body();
-        List<ServerAttribute> itemsList=new ArrayList<>();
-        Elements catalogs = body.select("ul[class=ty-menu__items cm-responsive-menu]");
-        for (Element catalog : catalogs.select("li[class=ty-menu__item cm-menu-item-responsive first-lvl]")) {
+        List<ServerAttribute> itemsList = new ArrayList<>();
+        for (Element catalog : getCatalog(body)) {
             if (nameCatalog.equals(catalog.select("span[class=menu-lvl-ctn exp-wrap]").select("bdi").text())) {
-                String nameDownCatalog=catalog.select("div.second-lvl").select("div.ty-menu__submenu-item-header").get(Integer.parseInt(id)).select("bdi").text();
+                String nameDownCatalog = catalog.select("div.second-lvl").select("div.ty-menu__submenu-item-header").get(Integer.parseInt(id)).select("bdi").text();
                 Element inputDownCatalogs = catalog.select("div.ty-menu__submenu-col").select("div.second-lvl").get(Integer.parseInt(id));
-                Elements items = inputDownCatalogs.select("div.ty-menu__submenu-list").select("div.ty-menu__submenu-item");
-//                System.out.println(inputDownCatalogs.select("div.ty-menu__submenu-list").select("div.ty-menu__submenu-item"));
-                for (Element item : items) {
+                for (Element item : getItems(inputDownCatalogs)) {
                     String typeItem = item.select("a").select("span").text();
                     String hrefItem = item.select("a").attr("href");
-                    itemsList.add(new ServerAttribute(nameDownCatalog,typeItem, hrefItem));
+                    itemsList.add(new ServerAttribute(nameDownCatalog, typeItem, hrefItem));
                 }
             }
         }
         return itemsList;
+    }
+
+    private Elements getCatalog(Element document) {
+        Elements catalogs = document.select("ul[class=ty-menu__items cm-responsive-menu]");
+        return catalogs.select("li[class=ty-menu__item cm-menu-item-responsive first-lvl]");
+    }
+
+    private Elements getItems(Element document){
+        return document.select("div.ty-menu__submenu-list").select("div.ty-menu__submenu-item");
+    }
+
+    public ServerAttribute getHrefItem(String nameCatalog, String nameDownCatalog, String name) throws IOException {
+        Document document = getDocument(HOME_PAGE);
+        Element body = document.body();
+        String hrefItem = null, typeItem = null;
+        List<ServerAttribute> itemsList = new ArrayList<>();
+        for (Element catalog : getCatalog(body)) {
+            if (nameCatalog.equals(catalog.select("span[class=menu-lvl-ctn exp-wrap]").select("bdi").text())) {
+                for (Element downCatalog : catalog.select("div.second-lvl")) {
+                    if (nameDownCatalog.equals(downCatalog.select("div.ty-menu__submenu-item-header").select("bdi").text())) {
+                        for (Element item : getItems(downCatalog)) {
+                            if (item.select("a").select("span").text().equals(name)) {
+                                typeItem = item.select("a").select("span").text();
+                                hrefItem = item.select("a").attr("href");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return new ServerAttribute(typeItem, hrefItem);
     }
 }
